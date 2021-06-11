fba650eccdeb9e548249dfd36274038af3558494
Sonu Keshri <Sonu_Keshri@amat.com>
2021-06-11 19:32:02 +0530

    First commit

---
+++ src/main/java/com/poc/utility/MathsUtil.java
@@ -0,0 +1,17 @@
+package com.poc.utility;
+
+public class MathsUtil {
+
+    public int sum(int a , int b) {
+        return a + b;
+    }
+
+
+    public int substract(int a , int b) {
+        return a - b;
+    }
+
+    public int multiply(int a , int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/com/poc/utility/MathsUtilTest.java
@@ -0,0 +1,33 @@
+package com.poc.utility;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MathsUtilTest {
+
+    public static MathsUtil util;
+
+    @BeforeAll
+    public static void init() {
+        util = new MathsUtil();
+    }
+
+    @Test
+    public void sumTest() {
+        System.out.println("----");
+        Assertions.assertEquals(5, util.sum(3,2));
+    }
+
+    @Test
+    public void substractTest() {
+        Assertions.assertEquals(1, util.substract(3,2));
+    }
+
+    @Test
+    public void multiplyTest() {
+        Assertions.assertEquals(6, util.multiply(3,2));
+    }
+}
