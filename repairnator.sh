8feb09ab6b77fade2d22c6ad0cfda0f726be4989
Sonu Keshri <Sonu_Keshri@amat.com>
2021-06-13 13:51:29 +0530

    Update clone repo path

--- repairnator.sh
+++ repairnator.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -cp repairnator-pipeline-3.3-SNAPSHOT-jar-with-dependencies.jar fr.inria.spirals.repairnator.pipeline.Launcher -l GIT_REPOSITORY  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 --repairTools NPEFix,AssertFixer,AstorJGenProg,AstorJKali,AstorJMut --gitrepo --gitrepourl https://github.com/sonukeshri/hackathon --gitrepobranch master -w /home/e165632/workspace_tmp
+java -cp repairnator-pipeline-3.3-SNAPSHOT-jar-with-dependencies.jar fr.inria.spirals.repairnator.pipeline.Launcher -l GIT_REPOSITORY  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 --repairTools NPEFix,AssertFixer,AstorJGenProg,AstorJKali,AstorJMut --gitrepo --gitrepourl https://github.com/sonukeshri/hackathon --gitrepobranch master -w /var/lib/jenkins/userContent/RepairnatorWorkspace
