7ef7495a4a4df8a74fe694122704ebdaa5a58e44
Sonu Keshri <Sonu_Keshri@amat.com>
2021-06-14 16:10:24 +0530

    solard fix

--- src/main/java/com/poc/entity/User.java
+++ src/main/java/com/poc/entity/User.java
@@ -3,6 +3,9 @@ package com.poc.entity;
 public class User {
 
     String greet(String name) {
+        String val = "hello";
+        String argStr = val.toString(); // Noncompliant
+        int argHash = val.hashCode(); // Noncompliant
         String capitalizedName = name.toUpperCase();
         return "Hello Mr./Ms "+capitalizedName;
     }
