af3672a15c4103826aef56c986c8978d5a6ddd47
repairnator <noreply@github.com>
2021-06-16 16:38:17 +0300

    Proposal for patching Sorald rule 2111

--- src/main/java/com/poc/entity/User.java
+++ src/main/java/com/poc/entity/User.java
@@ -24,8 +24,8 @@ public class User {
 
         double d = 1.1;
 
-        BigDecimal bd1 = new BigDecimal(d); // Noncompliant; see comment above
-        BigDecimal bd2 = new BigDecimal(1.1); // Noncompliant; same result
+        BigDecimal bd1 = BigDecimal.valueOf(d); // Noncompliant; see comment above
+        BigDecimal bd2 = BigDecimal.valueOf(1.1); // Noncompliant; same result
 
 
 
