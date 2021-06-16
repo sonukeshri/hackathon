ba74876709d7e7ee1e1bfdf3b36ea3d78a9c4942
Sonu Keshri <Sonu_Keshri@amat.com>
2021-06-16 19:31:42 +0530

    Add more sonarqube rules

--- src/main/java/com/poc/entity/User.java
+++ src/main/java/com/poc/entity/User.java
@@ -8,7 +8,7 @@ public class User {
     public static int x = 5;
     public String value;
 
-
+    /*1656: Variables should not be self-assigned*/
     public void setName(String value) {
         value = value;
     }
@@ -20,26 +20,31 @@ public class User {
         String argStr = x.toString(); // Noncompliant
         int argHash = x.hashCode(); // Noncompliant
 
-
+        System.out.println("Args Str "+argStr+" and ArgHash "+argHash);
 
         double d = 1.1;
 
         BigDecimal bd1 = new BigDecimal(d); // Noncompliant; see comment above
         BigDecimal bd2 = new BigDecimal(1.1); // Noncompliant; same result
 
-
+        System.out.println("Big Decimal values are "+bd1+" and "+bd2);
 
         int b;
-
         int i;
 
         i = 9 + 7; // Noncompliant; calculation result not used before value is overwritten
         i = compute();
 
+        b = i +8;
+
+        System.out.println("Value of b is "+b);
+
 
         float twoThirds = 2/3; // Noncompliant; int division. Yields 0.0
         long millisInYear = 1_000*3_600*24*365; // Noncompliant; int multiplication. Yields 1471228928
 
+        System.out.println("Two-Thirds is "+twoThirds);
+        System.out.println("MillisInYear "+millisInYear);
 
 
         String capitalizedName = name.toUpperCase();
