24da94519718012fe3f6e3d440d3b7348e1fda35
repairnator <noreply@github.com>
2021-06-16 16:38:17 +0300

    Proposal for patching Sorald rule 1854

--- src/main/java/com/poc/entity/User.java
+++ src/main/java/com/poc/entity/User.java
@@ -17,28 +17,16 @@ public class User {
 
 
         int [] x = new int[6];
-        String argStr = java.util.Arrays.toString(x); // Noncompliant
-        int argHash = java.util.Arrays.hashCode(x); // Noncompliant
 
 
 
         double d = 1.1;
-
-        BigDecimal bd1 = BigDecimal.valueOf(d); // Noncompliant; see comment above
-        BigDecimal bd2 = BigDecimal.valueOf(1.1); // Noncompliant; same result
 
 
 
         int b;
 
         int i;
-
-        i = 9 + 7; // Noncompliant; calculation result not used before value is overwritten
-        i = compute();
-
-
-        float twoThirds = 2/3; // Noncompliant; int division. Yields 0.0
-        long millisInYear = 1_000*3_600*24*365; // Noncompliant; int multiplication. Yields 1471228928
 
 
 
