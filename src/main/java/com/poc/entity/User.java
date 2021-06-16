fb480044e49ae7e8875e7b83adc187fd92cd2f4d
repairnator <noreply@github.com>
2021-06-16 15:46:20 +0300

    Proposal for patching Sorald rule 1854

--- src/main/java/com/poc/entity/User.java
+++ src/main/java/com/poc/entity/User.java
@@ -16,30 +16,16 @@ public class User {
 
         //21116 Arrays
         int [] x = new int[6];
-        String argStr = java.util.Arrays.toString(x); // Noncompliant
-        int argHash = java.util.Arrays.hashCode(x); // Noncompliant
 
 
         //21111 Big Decimal
         double d = 1.1;
-
-        BigDecimal bd1 = BigDecimal.valueOf(d); // Noncompliant; see comment above
-        BigDecimal bd2 = BigDecimal.valueOf(1.1); // Noncompliant; same result
 
         //1854 Unused assignments should be removed
 
         int b;
 
         int i;
-
-        i = 9 + 7; // Noncompliant; calculation result not used before value is overwritten
-        i = compute();
-
-        //2184: Math operands should be cast before
-        float twoThirds = 2/3; // Noncompliant; int division. Yields 0.0
-        long millisInYear = 1_000*3_600*24*365; // Noncompliant; int multiplication. Yields 1471228928
-        long bigNum = Integer.MAX_VALUE + 2; // Noncompliant. Yields -2147483647
-        long bigNegNum =  Integer.MIN_VALUE-1; //Noncompliant, gives a positive result instead of a negative one.
 
 
         String capitalizedName = name.toUpperCase();
